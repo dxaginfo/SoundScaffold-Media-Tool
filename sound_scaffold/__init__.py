15253b10c5a154f061cdeb771d509b1bbdee6a86
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-06-24 01:57:20 -0300

    Update __init__.py to include GeminiIntegration

--- sound_scaffold/__init__.py
+++ sound_scaffold/__init__.py
@@ -1,5 +1,6 @@
 from .audio_analyzer import AudioAnalyzer
 from .audio_enhancer import AudioEnhancer
 from .sound_library_manager import SoundLibraryManager
+from .gemini_integration import GeminiIntegration
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
